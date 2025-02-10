88fead4ed380673dbea28866e3d9e0073a8e6d2d
Ilya <Stepanenko.my-mail@yandex.ru>
2025-02-10 16:57:57 +1100

    Продолжение работы по подключению configureStore, эта тварь до сих пор не работает

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -1,4 +1,5 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 import { thunk } from 'redux-thunk'
 import reducerAdmin from './reducerAdmin.js';
 import reducerGeneralSearch from './reducerGeneralSearch.js'
@@ -12,6 +13,18 @@ let reducers = combineReducers({
 	searchResultsState: reducerSearchResults,
 });
 
-const store = createStore( reducers, applyMiddleware(thunk) );
+// const store = createStore( reducers, applyMiddleware(thunk) );
+
+const middleware = getDefaultMiddleware({
+	immutableCheck: false,
+	serializableCheck: false,
+	thunk: true,
+  });
+
+const store = configureStore({
+	reducer: { reducers },
+	middleware,
+	devTools: process.env.NODE_ENV !== 'production',
+});
 
 export default store;
