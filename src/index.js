d3e92bde1aeb82640714fed119cbefb01498ffd6
Ilya Stepanenko <stepanenko.my-mail@yandex.ru>
2025-03-11 01:45:06 +1100

    ---

--- src/index.js
+++ src/index.js
@@ -28,14 +28,14 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
     </Provider>
-    {/* <RouterProvider router={router} /> */}
-  </React.StrictMode>
+    // { <RouterProvider router={router} /> }
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
